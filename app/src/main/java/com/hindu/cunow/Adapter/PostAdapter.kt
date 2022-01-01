d08a970eb17c17828b33e8259d09cbae8c1a1438
Prajjwal Dubey <prajjwaldy@gamil.com>
2022-01-01 14:30:11 +0530

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/java/com/hindu/cunow/Adapter/PostAdapter.kt
    #	app/src/main/java/com/hindu/cunow/ui/home/HomeFragment.kt

--- app/src/main/java/com/hindu/cunow/Adapter/PostAdapter.kt
+++ app/src/main/java/com/hindu/cunow/Adapter/PostAdapter.kt
@@ -9,8 +9,14 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.annotation.NonNull
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import com.hindu.cunow.Model.PostModel
+import com.hindu.cunow.Model.UserModel
 import com.hindu.cunow.R
 import de.hdodenhof.circleimageview.CircleImageView
 
@@ -18,13 +24,7 @@ class PostAdapter(private val mContext: Context,
                   private val mPost:List<PostModel>):RecyclerView.Adapter<PostAdapter.ViewHolder>()
 {
                       private var firebaseUser:FirebaseUser? = null
-    inner class ViewHolder(@NonNull itemView: View):RecyclerView.ViewHolder(itemView){
-        val image: ImageView = itemView.findViewById(R.id.postImage)
-        val caption: TextView = itemView.findViewById(R.id.caption)
-        val publisherImage:CircleImageView = itemView.findViewById(R.id.publisher_profile_image)
-        val publisherName: TextView = itemView.findViewById(R.id.publisher_Name)
-        val varification:CircleImageView = itemView.findViewById(R.id.verification_image)
-    }
+
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater.from(mContext).inflate(R.layout.post_layout,parent,false)
@@ -35,10 +35,57 @@ class PostAdapter(private val mContext: Context,
 
         val post = mPost[position]
 
+        holder.bind(mPost[position],mContext)
+        publisher(holder.publisherImage,holder.publisherName,post.publisher!!,false,holder.verification)
+
 
     }
 
     override fun getItemCount(): Int {
         return mPost.size
     }
+
+
+    inner class ViewHolder(@NonNull itemView: View):RecyclerView.ViewHolder(itemView){
+        val image: ImageView = itemView.findViewById(R.id.postImage)
+        val caption: TextView = itemView.findViewById(R.id.caption)
+        val publisherImage:CircleImageView = itemView.findViewById(R.id.publisher_profile_image)
+        val publisherName: TextView = itemView.findViewById(R.id.publisher_Name)
+        val verification:CircleImageView = itemView.findViewById(R.id.verification_image)
+
+        fun bind(list:PostModel,context: Context){
+            caption.text = list.caption
+            if (list!!.image == ""){
+                image.visibility = View.GONE
+            }else{
+                Glide.with(context).load(list.image).into(image)
+            }
+        }
+    }
+
+    private fun publisher(profileImage:CircleImageView, name:TextView,publisherId:String,verification:Boolean,verifImage:CircleImageView){
+
+        val userDataRef = FirebaseDatabase.getInstance().reference.child("Users").child(publisherId)
+
+        userDataRef.addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                if (snapshot.exists()){
+                    val data = snapshot.getValue(UserModel::class.java)
+                    Glide.with(mContext).load(data!!.profileImage).into(profileImage)
+                    name.text = data.fullName
+                    if (verification){
+                        Glide.with(mContext).load(data.verifImage).into(verifImage)
+                    }else{
+                        verifImage.visibility =View.GONE
+                    }
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+
+        })
+    }
+
 }
--- app/src/main/java/com/hindu/cunow/ui/home/HomeFragment.kt
+++ app/src/main/java/com/hindu/cunow/ui/home/HomeFragment.kt
@@ -9,15 +9,31 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.google.android.material.progressindicator.LinearProgressIndicatorSpec
 import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import com.hindu.cunow.Adapter.PostAdapter
+import com.hindu.cunow.Model.PostModel
+import com.hindu.cunow.Model.UserModel
 import com.hindu.cunow.R
 import com.hindu.cunow.databinding.FragmentHomeBinding
 import kotlinx.android.synthetic.main.fragment_home.*
 import kotlinx.android.synthetic.main.fragment_home.view.*
 
 class HomeFragment : Fragment() {
+
+    var recyclerView: RecyclerView? = null
+    private var postAdapter: PostAdapter? = null
+    private var postList: MutableList<PostModel>? = null
+
+
 
     private lateinit var homeViewModel: HomeViewModel
     private var _binding: FragmentHomeBinding? = null
@@ -37,16 +53,11 @@ class HomeFragment : Fragment() {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        homeViewModel.text.observe(viewLifecycleOwner, Observer {
-
-            root.post_img.setOnClickListener { view->
-                postText(root)
-
-            }
-
-            root.post_txt.setOnClickListener { view->
-                postText(root)
-            }
+        homeViewModel.postModel!!.observe(viewLifecycleOwner, Observer {
+            initView(root)
+            postAdapter = context?.let { it1-> PostAdapter(it1,it) }
+            recyclerView!!.adapter = postAdapter
+            postAdapter!!.notifyDataSetChanged()
 
         })
         return root
@@ -68,6 +79,37 @@ class HomeFragment : Fragment() {
         Snackbar.make(view,"post added successfully",Snackbar.LENGTH_SHORT).show()
 
         caption_only.text.clear()
+    }
+    private fun initView(root:View){
+        recyclerView = root.findViewById(R.id.postRecyclerView) as RecyclerView
+        recyclerView!!.setHasFixedSize(true)
+        recyclerView!!.isNestedScrollingEnabled = false
+        val linearLayoutManager = LinearLayoutManager(context)
+        recyclerView!!.layoutManager = linearLayoutManager
+        loadUserImage(root)
+
+    }
+
+    private fun loadUserImage(root: View){
+        val dataRef = FirebaseDatabase
+            .getInstance().reference.child("Users")
+            .child(FirebaseAuth.getInstance().currentUser!!.uid)
+
+        dataRef.addListenerForSingleValueEvent(object : ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val listData = snapshot.getValue(UserModel::class.java)
+                if (listData!!.profileImage == ""){
+                    root.userProfileImg.visibility = View.GONE
+                }else{
+                    Glide.with(context!!).load(listData.profileImage).into(root.userProfileImg)
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+
+        })
     }
 
     override fun onDestroyView() {
