e794e5d41b77230e6645a6272b5243fda5a29cf8
Ashutosh <astcy@yahoo.com>
2022-03-05 22:44:23 +0530

    setting activity done

--- app/src/main/java/com/hindu/cunow/Activity/SettingActivity.kt
+++ app/src/main/java/com/hindu/cunow/Activity/SettingActivity.kt
@@ -1,7 +1,9 @@
 package com.hindu.cunow.Activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
 import com.hindu.cunow.R
 
 class SettingActivity : AppCompatActivity() {
@@ -9,7 +11,12 @@ class SettingActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_setting)
 
+        FirebaseAuth.getInstance().signOut()
 
+        val intent = Intent(this@SettingActivity,LogInActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
+        startActivity(intent)
+        finish()
 
     }
 }
