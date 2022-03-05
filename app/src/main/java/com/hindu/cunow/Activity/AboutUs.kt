529ca9c334f6c4ef0dca4a36949bf9c686ff5722
Ashutosh <astcy@yahoo.com>
2022-03-06 00:08:35 +0530

    setting activity done

---
+++ app/src/main/java/com/hindu/cunow/Activity/AboutUs.kt
@@ -0,0 +1,14 @@
+package com.hindu.cunow.Activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.hindu.cunow.R
+
+class AboutUs : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_about_us)
+
+
+    }
+}
