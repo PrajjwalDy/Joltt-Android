0f891e9a672e41b3dd562bc21408102b96b8ed9b
PrajjwalDy <21BCS2246@cuchd.in>
2022-03-06 15:31:04 +0530

    setting aboutUs logout completed

--- app/src/main/java/com/hindu/cunow/Activity/SettingActivity.kt
+++ app/src/main/java/com/hindu/cunow/Activity/SettingActivity.kt
@@ -17,6 +17,7 @@ class SettingActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
+
         logout.setOnClickListener {
             FirebaseAuth.getInstance().signOut()
 
