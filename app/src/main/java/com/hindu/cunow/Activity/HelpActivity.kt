1cad1c541ae13be03b34470c334d69dd427dbba0
Ashutosh <astcy@yahoo.com>
2022-03-15 19:24:46 +0530

    help request completed

--- app/src/main/java/com/hindu/cunow/Activity/HelpActivity.kt
+++ app/src/main/java/com/hindu/cunow/Activity/HelpActivity.kt
@@ -2,12 +2,39 @@ package com.hindu.cunow.Activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
 import com.hindu.cunow.R
+import kotlinx.android.synthetic.main.activity_help.*
+import kotlinx.android.synthetic.main.fragment_home.*
 
 class HelpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_help)
 
+        send_btn.setOnClickListener {
+            requestHelp()
+        }
+
     }
+
+    private fun requestHelp(){
+        val ref = FirebaseDatabase.getInstance().reference.child("Help")
+        val helpId = ref.push().key
+
+        val postMap = HashMap<String,Any>()
+        postMap["helpId"] = helpId!!
+        postMap["helpDescription"] = help_editText.text.toString()
+        postMap["helpReq"] = FirebaseAuth.getInstance().currentUser!!.uid
+
+        ref.child(helpId).updateChildren(postMap)
+
+        help_editText.text.clear()
+        Toast.makeText(this,"Help sent success",Toast.LENGTH_LONG).show()
+    }
+
 }
