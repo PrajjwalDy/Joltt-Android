6a3811b018ab32b2c21634b5ece1ce2091b6a8ba
Ashutosh <astcy@yahoo.com>
2022-03-20 22:05:50 +0530

    help request completed

--- app/src/main/java/com/hindu/cunow/Activity/VerificationRequest.kt
+++ app/src/main/java/com/hindu/cunow/Activity/VerificationRequest.kt
@@ -7,17 +7,40 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.FirebaseDatabase
 import com.hindu.cunow.R
 import kotlinx.android.synthetic.main.activity_help.*
+import kotlinx.android.synthetic.main.activity_verification_request.*
 
 class VerificationRequest : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_verification_request)
-        send_btn.setOnClickListener {
-            VerificationRequest()
+
+        requester_verification.setOnClickListener {
+            requestVerification()
         }
     }
 
+    private fun requestVerification() {
+        if(edit_TextAddress.text.isEmpty() || editTextAdhaar.text.isEmpty() ||  editTextUID.text.isEmpty()){
+            Toast.makeText(this,"Field must not be blank",Toast.LENGTH_LONG).show()
+        }else{
+            val ref = FirebaseDatabase.getInstance().reference.child("Verification_Request")
+            val requestId = ref.push().key
 
+            val postMap = HashMap<String, Any>()
+            postMap["requestId"] = requestId!!
+            postMap["fullName_verify"] = edit_TextAddress.text.toString()
+            postMap["adhaarNo"] = editTextAdhaar.text.toString()
+            postMap["uid_verify"] = editTextUID.text.toString()
+
+            ref.child(requestId).updateChildren(postMap)
+            edit_TextAddress.text.clear()
+            editTextAdhaar.text.clear()
+            editTextUID.text.clear()
+            Toast.makeText(this,"requested",Toast.LENGTH_LONG).show()
+        }
+
+
+    }
 
 
 
